e0841b1f5b41b6d8c214c93045744fc68924883f
FabioPin <fajos.pinheiro@hotmail.com>
2023-10-17 21:38:21 +0100

    added msw

---
+++ src/features/comments/commentsSlice.js
@@ -0,0 +1,42 @@
+// Import createAsyncThunk and createSlice here.
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+// Create loadCommentsForArticleId here.
+export const loadCommentsForArticleId = createAsyncThunk(
+  "comments/loadCommentsForArticleId",
+  async (articleId) => {
+    const response = await fetch(`api/articles/${articleId}/comments`);
+    const json = response.json();
+    return json;
+  }
+);
+
+// Create postCommentForArticleId here.
+
+const postCommentForArticleId = () => {};
+
+export const commentsSlice = createSlice({
+  name: "comments",
+  initialState: {
+    // Add initial state properties here.
+    byArticleId: {},
+    isLoadingComments: false,
+    failedToLoadComments: false,
+  },
+  // Add extraReducers here.
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadCommentsForArticleId.pending, (state) => {
+        state.isLoadingComments = true;
+        state.failedToLoadComments = false;
+      })
+      .addCase(loadCommentsForArticleId.fulfilled, (state) => {});
+  },
+});
+
+export const selectComments = (state) => state.comments.byArticleId;
+export const isLoadingComments = (state) => state.comments.isLoadingComments;
+export const createCommentIsPending = (state) =>
+  state.comments.createCommentIsPending;
+
+export default commentsSlice.reducer;
